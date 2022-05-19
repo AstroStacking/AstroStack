305909d65e4a00ab9093fc80fb3bd91df2cb9c3d
Matthieu Brucher <matthieu.brucher@gmail.com>
2022-05-19 22:51:09 +0100

    Plugin factory is now available

---
+++ src/Main/pluginfactory.cpp
@@ -0,0 +1,34 @@
+#include "pluginfactory.h"
+
+namespace astro
+{
+PluginFactory::PluginFactory() = default;
+
+PluginFactory& PluginFactory::get()
+{
+    static PluginFactory facility;
+    return facility;
+}
+
+const PluginFactory::PluginContainer& PluginFactory::getPluginForInterface(const std::string& interface)
+{
+    auto it = m_cache.find(interface);
+    if(it != m_cache.end())
+    {
+        return it->second;
+    }
+    auto& container = m_cache[interface];
+    const auto staticInstances = QPluginLoader::staticInstances();
+    for (QObject *plugin : staticInstances)
+    {
+        auto* obj = plugin->qt_metacast(interface.c_str());
+        if(obj != nullptr)
+        {
+            container.push_back(plugin);
+        }
+    }
+    
+    return container;
+}
+
+}
---
+++ src/Main/pluginfactory.h
@@ -0,0 +1,27 @@
+#include <QtCore/QPluginLoader>
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace astro
+{
+class PluginFactory
+{
+    PluginFactory();
+  public:
+    static PluginFactory& get();
+
+    using PluginContainer = std::vector<QObject*>;
+
+    template<typename T>
+    const PluginContainer& getPluginsFor()
+    {
+        return getPluginForInterface(qobject_interface_iid<T>());
+    }
+    
+  protected:
+    const PluginContainer& getPluginForInterface(const std::string& interface);
+    std::unordered_map<std::string, PluginContainer> m_cache;
+};
+}
