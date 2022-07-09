599a3174a27bb288a067bb683f5621153ecd9b4a
Matthieu Brucher <matthieu.brucher@gmail.com>
2022-07-09 12:10:03 +0100

    Adding a bunch of conjugated functions

--- src/Algorithms/Algos/optim/step/conjugate_gradient.h
+++ src/Algorithms/Algos/optim/step/conjugate_gradient.h
@@ -18,17 +18,52 @@ public:
     {
         Eigen::VectorXd gradient = fun.gradient(x);
 
-        if(state.getDirection().size() > 0)
+        if (state.getDirection().size() > 0)
         {
             double coeff = Conjugated::conjugate(gradient, state.getGradient(), state.getDirection());
             state.setGradient(gradient);
             state.setData("conjugate_coefficient", coeff);
-            return - gradient + coeff * state.getDirection();
+            return -gradient + coeff * state.getDirection();
         }
         state.setData("conjugate_coefficient", 0);
         state.setGradient(gradient);
         return -gradient;
+    }
+};
 
+/// The Crowder-Wolfe or Hestenes-Stiefel formula
+class CWConjugate
+{
+public:
+    static double conjugate(const Eigen::VectorXd& newGradient, const Eigen::VectorXd& oldGradient,
+                            const Eigen::VectorXd& direction)
+    {
+        return static_cast<double>(newGradient.transpose() * (newGradient - oldGradient)) /
+               static_cast<double>(direction.transpose() * (newGradient - oldGradient));
+    }
+};
+
+/// The Dixon formula
+class DConjugate
+{
+public:
+    static double conjugate(const Eigen::VectorXd& newGradient, const Eigen::VectorXd& oldGradient,
+                            const Eigen::VectorXd& direction)
+    {
+        return -static_cast<double>(newGradient.transpose() * newGradient) /
+               static_cast<double>(direction.transpose() * oldGradient);
+    }
+};
+
+/// The Dai Yan formula
+class DYConjugate
+{
+public:
+    static double conjugate(const Eigen::VectorXd& newGradient, const Eigen::VectorXd& oldGradient,
+                            const Eigen::VectorXd& direction)
+    {
+        return static_cast<double>(newGradient.transpose() * newGradient) /
+               static_cast<double>(direction.transpose() * (newGradient - oldGradient));
     }
 };
 } // namespace step
--- src/Algorithms/Tests/optim/step/conjugate_gradient.cpp
+++ src/Algorithms/Tests/optim/step/conjugate_gradient.cpp
@@ -6,29 +6,114 @@
 
 namespace
 {
+class Function
+{
+public:
+    double operator()(const Eigen::VectorXd& X) const { return std::pow(X(0) - 2, 2) + std::pow(2 * X(1) + 4, 2); }
+
+    Eigen::VectorXd gradient(const Eigen::VectorXd& X) const
+    {
+        Eigen::VectorXd result(2);
+        result << 2. * (X(0) - 2), 4 * (2 * X(1) + 4);
+
+        return result;
+    }
+};
+
 class Simple
 {
 public:
-    static double conjugate(const Eigen::VectorXd& newGadient, const Eigen::VectorXd& oldGadient, const Eigen::VectorXd& direction)
+    static double conjugate(const Eigen::VectorXd& newGradient, const Eigen::VectorXd& oldGradient,
+                            const Eigen::VectorXd& direction)
     {
         return 0;
     }
 };
 
-}
+constexpr double ARTIFICIAL_FIRST_STEP = 0.178571428571;
 
-TEST(ConjugateGradient, Powell)
+} // namespace
+
+TEST(ConjugateGradient, Simple)
 {
     optim::State state;
     optim::step::ConjugateGradient<Simple> gradient;
-    Powell fun;
+    Function fun;
 
-    Eigen::VectorXd x(4);
-    x << 0, 0, 0, 0;
+    Eigen::VectorXd x(2);
+    x << 0, 0;
     ASSERT_EQ(gradient(x, state, fun), -fun.gradient(x));
     ASSERT_EQ(state.getGradient(), fun.gradient(x));
+    ASSERT_EQ(state.getData("conjugate_coefficient"), 0);
 
-    x << 1, 1, 0, 0;
+    x << 1, 1;
     ASSERT_EQ(gradient(x, state, fun), -fun.gradient(x));
     ASSERT_EQ(state.getGradient(), fun.gradient(x));
+    ASSERT_EQ(state.getData("conjugate_coefficient"), 0);
+}
+
+TEST(ConjugateGradient, CWConjugate)
+{
+    optim::State state;
+    optim::step::ConjugateGradient<optim::step::CWConjugate> gradient;
+    Function fun;
+
+    Eigen::VectorXd x(2);
+    x << 0, 0;
+    auto step = gradient(x, state, fun);
+    ASSERT_EQ(step, -fun.gradient(x));
+    ASSERT_EQ(state.getGradient(), fun.gradient(x));
+    ASSERT_EQ(state.getData("conjugate_coefficient"), 0);
+
+    x = ARTIFICIAL_FIRST_STEP * step;
+    state.setDirection(x);
+    auto newStep = gradient(x, state, fun);
+    ASSERT_NE(newStep, -fun.gradient(x));
+    ASSERT_EQ(state.getGradient(), fun.gradient(x));
+    ASSERT_NE(state.getData("conjugate_coefficient"), 0);
+    ASSERT_LT(fun(x + 0.01 * newStep), fun(x));
+}
+
+TEST(ConjugateGradient, DConjugate)
+{
+    optim::State state;
+    optim::step::ConjugateGradient<optim::step::DConjugate> gradient;
+    Function fun;
+
+    Eigen::VectorXd x(2);
+    x << 0, 0;
+    auto step = gradient(x, state, fun);
+    ASSERT_EQ(step, -fun.gradient(x));
+    ASSERT_EQ(state.getGradient(), fun.gradient(x));
+    ASSERT_EQ(state.getData("conjugate_coefficient"), 0);
+
+    x = ARTIFICIAL_FIRST_STEP * step;
+    state.setDirection(x);
+    auto newStep = gradient(x, state, fun);
+    ASSERT_NE(newStep, -fun.gradient(x));
+    ASSERT_EQ(state.getGradient(), fun.gradient(x));
+    ASSERT_NE(state.getData("conjugate_coefficient"), 0);
+    ASSERT_LT(fun(x + 0.001 * newStep), fun(x));
+}
+
+TEST(ConjugateGradient, DYConjugate)
+{
+    optim::State state;
+    optim::step::ConjugateGradient<optim::step::DYConjugate> gradient;
+    Function fun;
+
+    Eigen::VectorXd x(2);
+    x << 0, 0;
+    auto step = gradient(x, state, fun);
+    ASSERT_EQ(step, -fun.gradient(x));
+    ASSERT_EQ(state.getGradient(), fun.gradient(x));
+    ASSERT_EQ(state.getData("conjugate_coefficient"), 0);
+
+    x = ARTIFICIAL_FIRST_STEP * step;
+    state.setDirection(x);
+    auto newStep = gradient(x, state, fun);
+    ASSERT_NE(newStep, -fun.gradient(x));
+    ASSERT_EQ(state.getGradient(), fun.gradient(x));
+    ASSERT_NE(state.getData("conjugate_coefficient"), 0);
+    ASSERT_LT(fun(x + 0.01 * newStep), fun(x));
 }
