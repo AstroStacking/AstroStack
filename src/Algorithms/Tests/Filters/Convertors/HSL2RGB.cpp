965af9fafc64b54dd1b1120083bc55100db92a52
Matthieu Brucher <matthieu.brucher@gmail.com>
2022-07-30 21:29:23 +0100

    More HSL2RGB tests

--- src/Algorithms/Tests/Filters/Convertors/HSL2RGB.cpp
+++ src/Algorithms/Tests/Filters/Convertors/HSL2RGB.cpp
@@ -24,3 +24,45 @@ TEST(HSL2RGB, white)
     ASSERT_EQ(rgb[1], 1);
     ASSERT_EQ(rgb[2], 1);
 }
+
+TEST(HSL2RGB, red)
+{
+    itk::RGBPixel<double> hsl;
+    hsl[0] = 0;
+    hsl[1] = 1;
+    hsl[2] = .5;
+
+    auto rgb = astro::filters::convertors::HSL2RGB(hsl);
+    
+    ASSERT_EQ(rgb[0], 1);
+    ASSERT_EQ(rgb[1], 0);
+    ASSERT_EQ(rgb[2], 0);
+}
+
+TEST(HSL2RGB, green)
+{
+    itk::RGBPixel<double> hsl;
+    hsl[0] = 120;
+    hsl[1] = 1;
+    hsl[2] = .5;
+
+    auto rgb = astro::filters::convertors::HSL2RGB(hsl);
+    
+    ASSERT_EQ(rgb[0], 0);
+    ASSERT_EQ(rgb[1], 1);
+    ASSERT_EQ(rgb[2], 0);
+}
+
+TEST(HSL2RGB, blue)
+{
+    itk::RGBPixel<double> hsl;
+    hsl[0] = 240;
+    hsl[1] = 1;
+    hsl[2] = .5;
+
+    auto rgb = astro::filters::convertors::HSL2RGB(hsl);
+    
+    ASSERT_EQ(rgb[0], 0);
+    ASSERT_EQ(rgb[1], 0);
+    ASSERT_EQ(rgb[2], 1);
+}
