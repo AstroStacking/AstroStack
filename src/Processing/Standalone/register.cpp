cc7babd7681c7596a202f9b619fcffbe992f77c4
Matthieu Brucher <matthieu.brucher@gmail.com>
2022-08-13 22:28:15 +0100

    Stay the same for now

--- src/Processing/Standalone/register.cpp
+++ src/Processing/Standalone/register.cpp
@@ -10,14 +10,17 @@
 int main(int argc, char** argv)
 {
     QCoreApplication app(argc, argv);
-    QCoreApplication::setApplicationName("Stacking");
+    QCoreApplication::setApplicationName("Register");
     QCoreApplication::setApplicationVersion("0.2.0");
 
     QCommandLineParser parser;
-    parser.setApplicationDescription("Stacking");
+    parser.setApplicationDescription("Register");
     parser.addHelpOption();
     parser.addVersionOption();
-    parser.addPositionalArgument("images", QCoreApplication::translate("main", "Input images."));
+    QCommandLineOption input1Option("input1", QCoreApplication::translate("main", "Input image."), "input1");
+    parser.addOption(input1Option);
+    QCommandLineOption input2Option("input2", QCoreApplication::translate("main", "Input image on which to register."), "input2");
+    parser.addOption(input2Option);
     QCommandLineOption outputOption("output", QCoreApplication::translate("main", "Output image."), "output");
     parser.addOption(outputOption);
     QCommandLineOption highdefOption("high-def", QCoreApplication::translate("main", "Save in 16bits."));
@@ -25,22 +28,25 @@ int main(int argc, char** argv)
 
     // Process the actual command line arguments given by the user
     parser.process(app);
+    if (!parser.isSet(input1Option))
+    {
+        throw std::runtime_error("Missing input1 image");
+    }
+    if (!parser.isSet(input2Option))
+    {
+        throw std::runtime_error("Missing input2 image");
+    }
     if (!parser.isSet(outputOption))
     {
         throw std::runtime_error("Missing output image");
     }
 
-    QStringList filenames = parser.positionalArguments();
+    std::string input1 = parser.value(input1Option).toStdString();
+    std::string input2 = parser.value(input2Option).toStdString();
     std::string output = parser.value(outputOption).toStdString();
     bool highdef = parser.isSet(highdefOption);
 
-    std::vector<astro::AstroImage> images;
-    for (QString filename : filenames)
-    {
-        images.push_back(astro::enrichImage(filename.toStdString(), astro::io::open(filename.toStdString())));
-    }
-
-    auto img = astro::processing::maxStacking(images);
+    astro::AstroImage img = astro::enrichImage(input1, astro::io::open(input1));
 
     if (highdef)
     {
