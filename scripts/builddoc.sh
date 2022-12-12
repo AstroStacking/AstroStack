37e98a81274bdc7b40aeac33e34da6327298ad33
Matthieu Brucher <matthieu.brucher@gmail.com>
2022-12-12 22:33:54 +0000

    #13 adding required link, but need newer pandoc than 2.19.2

--- scripts/builddoc.sh
+++ scripts/builddoc.sh
@@ -2,5 +2,6 @@
 pandoc -o doc/AstroStack.pdf README.md */*.md src/*/*/*.md -V colorlinks=true \
                                        -V linkcolor=blue \
                                        -V urlcolor=red \
-                                       -V toccolor=gray
+                                       -V toccolor=gray \
+                                       --file-scope
 
