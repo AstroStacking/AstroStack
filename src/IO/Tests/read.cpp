eafce854a3ce6958e511f541737464162a326345
Matthieu Brucher <matthieu.brucher@gmail.com>
2022-12-21 18:18:51 +0000

    Starting tests for Processing and IO

--- src/IO/Tests/read.cpp
+++ src/IO/Tests/read.cpp
@@ -1,5 +1,30 @@
-
 #include <IO/hdf5.h>
+
+#include <QtCore/QTemporaryFile>
 
 #include <gtest/gtest.h>
 
+class HDF5Test : public ::testing::Test
+{
+
+protected:
+    QTemporaryFile temp;
+    H5::H5File file;
+
+    HDF5Test() = default;
+
+    ~HDF5Test() override = default;
+
+    void SetUp() override
+    {
+        if (temp.open())
+        {
+            temp.close();
+            file = H5::H5File(temp.fileName().toStdString(), H5F_ACC_TRUNC);
+        }
+    }
+
+    void TearDown() override {}
+};
+
+TEST_F(HDF5Test, CanDoBar) {}
--- src/Processing/Tests/grey.cpp
+++ src/Processing/Tests/grey.cpp
@@ -2,4 +2,3 @@
 #include <Processing/grey.h>
 
 #include <gtest/gtest.h>
-
