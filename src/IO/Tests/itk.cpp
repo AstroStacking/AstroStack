01960a56adacebb19634070c03a6724d74eb5067
Matthieu Brucher <matthieu.brucher@gmail.com>
2022-12-22 16:17:21 +0000

    Adding colored tests

--- src/IO/Tests/itk.cpp
+++ src/IO/Tests/itk.cpp
@@ -5,7 +5,7 @@
 
 #include <gtest/gtest.h>
 
-TEST(ITK, Input)
+TEST(ITK, InputBlack)
 {
     auto img = astro::io::open("black.png");
     itk::Size<astro::Dimension> size{20, 10};
@@ -25,7 +25,7 @@ TEST(ITK, Input)
     }
 }
 
-TEST(ITK, Output)
+TEST(ITK, OutputBlack)
 {
     auto img = astro::io::open("black.png");
     astro::io::save<uint8_t>(img, "black_bis.png");
@@ -45,5 +45,57 @@ TEST(ITK, Output)
         auto value = it.Get();
         ASSERT_EQ(value, black);
         ++it;
+    }
+}
+
+TEST(ITK, InputGradient)
+{
+    auto img = astro::io::open("gradient.png");
+    itk::Size<astro::Dimension> size{256, 3};
+    ASSERT_EQ(img->GetRequestedRegion().GetSize(), size);
+
+    astro::PixelType black(0.f);
+
+    using IteratorType = itk::ImageRegionIterator<astro::ImageType>;
+    IteratorType it(img, img->GetRequestedRegion());
+    it.GoToBegin();
+    int pixel = 0;
+
+    while (!it.IsAtEnd())
+    {
+        auto value = it.Get();
+        int i = pixel / 256;
+        int j = pixel % 256;
+        for (int k = 0; k < 3; ++k)
+        {
+            ASSERT_NEAR(value[k], k == i ? j / 255. : 0, 0.00001);
+        }
+        ++it;
+        ++pixel;
+    }
+}
+
+TEST(ITK, InputGradientGreyed)
+{
+    auto img = astro::io::open("gradient_greyed.png");
+    itk::Size<astro::Dimension> size{256, 3};
+    ASSERT_EQ(img->GetRequestedRegion().GetSize(), size);
+
+    using IteratorType = itk::ImageRegionIterator<astro::ImageType>;
+    IteratorType it(img, img->GetRequestedRegion());
+    it.GoToBegin();
+    int pixel = 0;
+
+    while (!it.IsAtEnd())
+    {
+        auto value = it.Get();
+        int i = pixel / 256;
+        int j = pixel % 256;
+        for (int k = 0; k < 3; ++k)
+        {
+            ASSERT_NEAR(value[k], j / 255., 0.00001);
+        }
+        ++it;
+        ++pixel;
     }
 }
