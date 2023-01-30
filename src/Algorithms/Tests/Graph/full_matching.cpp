c94cc46b057773afc131a19d097ff90c4afbb054
Matthieu Brucher <matthieu.brucher@gmail.com>
2023-01-30 19:57:57 +0000

    Proper angle

--- src/Algorithms/Tests/Graph/full_matching.cpp
+++ src/Algorithms/Tests/Graph/full_matching.cpp
@@ -15,17 +15,17 @@ TEST(Graph_FullMatching, no_match)
 TEST(Graph_FullMatching, some_match)
 {
     std::vector<std::pair<double, double>> graph1{{0, 0}, {1, 1}, {2, 0}, {1, -1}, {100, 200}, {1000, 2000}};
-    std::vector<std::pair<double, double>> graph2{{0, 0}, {-1, -4}, {2, 0}, {1, -1}, {-100, 200}, {1000, -2000}};
+    std::vector<std::pair<double, double>> graph2{{0, 0}, {-1, -1}, {6, 0}, {1, -1}, {-100, 200}, {1000, -2000}};
 
     auto result = astro::graph::matchFullGraph(graph1, graph2, 3, 0.001);
 
     ASSERT_EQ(result.size(), 3);
     ASSERT_EQ(result[0].first, 0);
-    ASSERT_EQ(result[0].second, 0);
+    ASSERT_EQ(result[0].second, 1);
     ASSERT_EQ(result[1].first, 1);
-    ASSERT_EQ(result[1].second, 3);
+    ASSERT_EQ(result[1].second, 0);
     ASSERT_EQ(result[2].first, 2);
-    ASSERT_EQ(result[2].second, 2);
+    ASSERT_EQ(result[2].second, 3);
 }
 
 TEST(Graph_DistanceMatrix, simple)
